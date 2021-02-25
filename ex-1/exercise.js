2ed5efb0a3c9003468d4797f70effa51596470e7
Rawah94 <78372274+Rawah94@users.noreply.github.com>
2021-02-25 11:50:30 +0100

    Update exercise.js

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,2 +1,12 @@
 // Example
 window.alert("Hello World!");
+\\
+var x = "position"
+var y = "place"
+var z = "spouse"
+var n = number
+
+
+window.alert("You will be a position in place, and married to spouse with number kids");
+
+consol.log(You will be a X in Y, and married to Z with N kids)

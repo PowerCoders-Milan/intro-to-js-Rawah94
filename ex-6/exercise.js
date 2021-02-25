d5bbe13f9cfb44d2d516c35d40d289597f1f2b88
Rawah94 <78372274+Rawah94@users.noreply.github.com>
2021-02-25 21:50:37 +0100

    Update exercise.js

--- ex-6/exercise.js
+++ ex-6/exercise.js
@@ -1,2 +1,8 @@
-// Example
-window.alert("Hello World!");
+
+var Q = prompt("Quote of the day?")
+
+var Author = Dante Alighieri
+var "-Quote-" = "-“The hottest places in hell are reserved for those who, in times of great moral crisis, maintain their neutrality.”-
+
+
+window.alert(+-Author-+ says, + "-Quote-"+ )

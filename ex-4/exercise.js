3972bba1509329366ea22de2bad40279baa64cba
Rawah94 <78372274+Rawah94@users.noreply.github.com>
2021-02-25 17:36:50 +0100

    Update exercise.js

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,2 +1,11 @@
-// Example
-window.alert("Hello World!");
+var name = prompt("what's your name?")
+
+window.alert(" hello " + name)
+
+var day = prompt("how was your day?")
+
+if (day === "good") {
+    window.alert("I'm happy for you champ")
+} else if (day === "bad") {
+    window.alert("That's too bad, tomorrow it's a new day")
+}

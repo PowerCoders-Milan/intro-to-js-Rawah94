406ca90c6525cb74f08bebc7c26907289b9b6707
Rawah G <rawah.h@outlook.com>
2021-02-25 17:13:24 +0100

    :updated

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,12 @@
-// Example
-window.alert("Hello World!");
+var x = 89;
+var y = 57;
+
+
+
+if (y > x) {
+    window.alert(y + " is greater than " + x);
+} else if (x > y) {
+    window.alert(x + " is greater than " + y);
+} else {
+    window.alert("The numbers are the same.")
+}
